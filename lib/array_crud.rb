d9a298d515ed4580483f596a031d7f4df7832c48
kelvin lanier <kelvinlanier@yahoo.com>
2020-07-28 22:33:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,3 @@
-def create_an_empty_array
-  
-end
 
-def create_an_array
-  ["keisha","jessica","mary","monica"]
-end
 
-def add_element_to_end_of_array(girls,harriet,juliet,susy)
-  add_element_to_end_of_array.push("element2")
-end
 
-def add_element_to_start_of_array(array, element)
-  
-end
-
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
-end
